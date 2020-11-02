6c4059f0f0f6a5621b98d82971b72912c8167ec2
xQuadDev <developer@quad.finance>
2020-11-02 21:42:53 +0100

    added migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,23 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+const QuadChamber = artifacts.require("QuadChamber");
+const LPChamber = artifacts.require("LPChamber");
+const PoolGovernor = artifacts.require("PoolGovernor");
+const KillswitchGovernor = artifacts.require("KillswitchGovernor");
+const FeeGovernor = artifacts.require("FeeGovernor");
+
+const quadAdmin = ""; //QuadAdmin's address goes here
+
+module.exports = async function (deployer, network) {
+    //network test skips migrations
+    if (network == "test")
+        return;
+
+    await deployer.deploy(Migrations);
+
+    await deployer.deploy(QuadChamber, 500, 5100, 60 * 60 * 48, 500, quadAdmin);
+    await deployer.deploy(LPChamber, 2000, 5100, 60 * 60 * 48, quadAdmin);
+    await deployer.deploy(PoolGovernor, quadAdmin);
+    await deployer.deploy(KillswitchGovernor, quadAdmin);
+    await deployer.deploy(FeeGovernor, quadAdmin);
 };

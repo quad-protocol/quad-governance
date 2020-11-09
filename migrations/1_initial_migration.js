9f034055c20348af41fff4e75fdc5b1b06fad65b
xQuadDev <developer@quad.finance>
2020-11-09 22:36:48 +0100

    Need to deploy migrations regardless

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -9,11 +9,11 @@ const FeeGovernor = artifacts.require("FeeGovernor");
 const quadAdmin = ""; //QuadAdmin's address goes here
 
 module.exports = async function (deployer, network) {
+    await deployer.deploy(Migrations);
+
     //network test skips migrations
     if (network == "test")
         return;
-
-    await deployer.deploy(Migrations);
 
     await deployer.deploy(QuadChamber, 500, 5100, 60 * 60 * 48, 500, quadAdmin);
     await deployer.deploy(LPChamber, 2000, 5100, 60 * 60 * 48, quadAdmin);
